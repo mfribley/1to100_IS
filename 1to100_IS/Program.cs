5fd7989363fc3d3e13cd893c5445162746740eeb
mfribley <mfribley@mail.usf.edu>
2019-01-23 23:11:23 -0500

    Add project files.

---
+++ 1to100_IS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1to100_IS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
