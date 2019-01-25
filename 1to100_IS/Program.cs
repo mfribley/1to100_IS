b03735209e3bec677bc0113ff3ab6b8326234034
mfribley <mfribley@mail.usf.edu>
2019-01-24 19:51:06 -0500

    1 to 100 - Version 3

--- 1to100_IS/Program.cs
+++ 1to100_IS/Program.cs
@@ -2,10 +2,8 @@
    Author: Mikessa Fribley
    Date: 1/22/2019
    Comments: This C# Console application code demonstrates the use of
-             iterative statements after getting input from users.  
+             an iterative statement after getting input from users.  
 */
-
-
 
 using System;
 
@@ -18,13 +16,28 @@ namespace _1to100_IS
             // Ask the user for input.
             Console.WriteLine("Enter an integer between 1 and 100.");
 
+            /* 
+               The try catch block will validate user input.
+               If an user provides bad input, the catch block
+               will handle the error and a message will be displayed.
+            */
             try
             {
+                // This variable will gather data from user input.
                 string Input = Console.ReadLine();
 
+                /* 
+                   This variable will be used to store the value of user input
+                   after the input variable is parsed to integer.
+                */
                 int ValueOfInput = int.Parse(Input);
                 int VOI = 1;
 
+                /*
+                   This IF/ELSE IF/ELSE statement is used to conditionally
+                   test the value of users input. A Do While Loop will be
+                   executed for the values entered.
+                */
                 if ((ValueOfInput > 0) && (ValueOfInput <= 50))
                 {
                     do
@@ -33,8 +46,8 @@ namespace _1to100_IS
                         ". This is the current integer value in the loop: " + VOI++);
                     } while (ValueOfInput > 0);
 
-                    Console.WriteLine("Press the 'Enter' key to exit the application.");
-                    while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.WriteLine("Press any key to exit the application.");
+                    Console.ReadKey(true);
                 }
 
                 else if ((ValueOfInput > 50) && (ValueOfInput <= 100))
@@ -45,23 +58,23 @@ namespace _1to100_IS
                         ". This is the current integer value in the loop: " + VOI++);
                     } while (ValueOfInput > 0);
 
-                    Console.WriteLine("Press the 'Enter' key to exit the application.");
-                    while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.WriteLine("Press any key to exit the application.");
+                    Console.ReadKey(true);
                 }
 
                 else
                 {
                     Console.WriteLine("Enter an integer between 1 and 100 next time.");
-                    Console.WriteLine("Press the 'Enter' key to exit the application.");
-                    while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.WriteLine("Press any key to exit the application.");
+                    Console.ReadKey(true);
                 }
             } // End of try.
 
             catch
             {
                 Console.WriteLine("Please use an integer data type next time.");
-                Console.WriteLine("Press the 'Enter' key to exit the application.");
-                while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                Console.WriteLine("Press any key to exit the application.");
+                Console.ReadKey(true);
             } // End of catch.
         } // End of main.
     } // End of class.
